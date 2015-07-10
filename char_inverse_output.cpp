f75d907c0f173d1e78e2f79ae3137fdc08e9126a
lixinming913 <xinyu913@163.com>
2015-07-10 16:06:18 +0800

    inverse char to output

---
+++ char_inverse_output.cpp
@@ -0,0 +1,27 @@
+/*
+ * 将字符串反转输出
+ */
+
+
+#include <iostream>
+using namespace std;
+ 
+ 
+void 
+main()
+{
+	char a[50];
+	int i=0,j;
+	char temp;
+
+	memset(a, 0, sizeof(a)); /*void *memset( void *s, int ch, size_t n) 将s中前n个字节替换为ch并返回s*/
+	cin.getline(a, 50, '\n');    /*一次读取多个字符，cin.getline(字符指针（char *），字符个数，结束符)*/
+
+	for(i = 0, j = strlen(a) - 1; i < strlen(a) / 2; i++, j--){ 
+		temp = a[i];  
+		a[i] = a[j];
+		a[j] = temp;  
+	}
+	
+	cout << a << endl; 
+}
