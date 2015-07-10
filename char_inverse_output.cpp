09a5fe331beeb2827eede9132f6082b1c7c4ea99
lixinming913 <xinyu913@163.com>
2015-07-10 16:08:04 +0800

    modify explaination

--- char_inverse_output.cpp
+++ char_inverse_output.cpp
@@ -15,7 +15,7 @@ main()
 	char temp;
 
 	memset(a, 0, sizeof(a)); /*void *memset( void *s, int ch, size_t n) 将s中前n个字节替换为ch并返回s*/
-	cin.getline(a, 50, '\n');    /*一次读取多个字符，cin.getline(字符指针（char *），字符个数，结束符)*/
+	cin.getline(a, 50, '\n');/*一次读取多个字符，cin.getline(字符指针（char *），字符个数，结束符)*/
 
 	for(i = 0, j = strlen(a) - 1; i < strlen(a) / 2; i++, j--){ 
 		temp = a[i];  
